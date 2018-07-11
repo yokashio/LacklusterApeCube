4675119eba21502a9983d9add159c30cb76a809e
yokashio <eblaser@uncc.edu>
2018-07-11 13:41:34 -0400

    Trying to add tilt
    gliding tests

---
+++ LacklusterApeCube/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    private CharacterController controller;
+
+    private float baseSpeed = 10.0f;
+    private float rotSpeedX = 3.0f;
+    private float rotSpeedY = 1.5f;
+
+	// Use this for initialization
+	void Start () {
+        controller = GetComponent<CharacterController>();	
+	}
+
+    // Update is called once per frame
+    void Update () {
+
+        Vector3 moveVector = transform.forward;
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 inputs = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        Vector3 yaw = inputs.x * transform.right * rotSpeedX * Time.deltaTime;
+        Vector3 pitch = inputs.z * -transform.up * rotSpeedY * Time.deltaTime;
+        Vector3 dir = yaw + pitch;
+
+        float maxX = Quaternion.LookRotation(moveVector + dir).eulerAngles.x;
+        if (maxX < 90 && maxX > 70 || maxX > 270 && maxX < 290)
+        {
+        }
+        else
+        {
+            moveVector += dir;
+
+            transform.rotation = Quaternion.LookRotation(moveVector);
+
+        
+        }
+        controller.Move(moveVector * Time.deltaTime);
+    }
+
+
+}
