0c40ec3fac451b75fdd6d1a13f1e702057a7ca07
yokashio <eblaser@uncc.edu>
2018-07-12 03:26:47 -0400

    working on glide

---
+++ LLAC/Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    public Transform lookAt;
+
+    private Vector3 desiredPosition;
+    private float offset = 1.5f;
+    private float distance = 6.5f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        desiredPosition = lookAt.position + (-transform.forward * distance) + (transform.up * offset);
+        transform.position = Vector3.Lerp(transform.position, desiredPosition, 0.2f);
+
+        transform.LookAt(lookAt.position + (Vector3.up * offset));
+    }
+}
---
+++ LLAC/Assets/Scripts/RotateModel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateModel : MonoBehaviour {
+    
+    [SerializeField] private Transform player;
+    float smooth = 4.0f;
+    float tiltAngle = 60.0f;
+
+    void Update()
+    {
+        // Smoothly tilts a transform towards a target rotation.
+        float tiltAroundZ = Input.GetAxis("Horizontal") * tiltAngle*3/4;
+        float tiltAroundY = Input.GetAxis("Horizontal") * tiltAngle/2;
+        float tiltAroundX = Input.GetAxis("Vertical") * tiltAngle;
+
+        Quaternion target = (Quaternion.Euler(tiltAroundX, tiltAroundY, -tiltAroundZ) * player.localRotation);
+
+        // Dampen towards the target rotation
+        transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * smooth);
+
+        if (Input.GetKey(KeyCode.Alpha1))
+        {
+        }
+    }
+}
