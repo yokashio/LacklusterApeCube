67d2c85916fc34cda6ce37fb87107f62c7dc033b
yokashio <eblaser@uncc.edu>
2018-07-19 01:35:20 -0400

    committ elliott

--- LLAC/Assets/Scripts/PlayerMovement.cs
+++ LLAC/Assets/Scripts/PlayerMovement.cs
@@ -6,40 +6,80 @@ public class PlayerMovement : MonoBehaviour {
 
     private CharacterController controller;
 
-    private float baseSpeed = 2.0f;
-    private float rotSpeedX = 3.0f;
-    private float rotSpeedY = 1.5f;
+    private float baseSpeed = 5.0f;
+    private float rotSpeedX = 4.0f;
+    private float rotSpeedY = 4.0f;
+    private float gravity = 2.0f;
+    private float cameraDist = 8.0f;
+    private float cameraOffset = 3.0f;
+    Vector3 moveVector;
+
+    [SerializeField] private bool isBall;
 
     // Use this for initialization
     void Start () {
         controller = GetComponent<CharacterController>();
+        //moveVector = baseSpeed * transform.forward;
     }
-	
-	// Update is called once per frame
-	void Update () {
+    // Update is called once per frame
+    void FixedUpdate() {
 
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
+        float moveYaw = Input.GetAxis("Horizontal");
+        float movePitch = Input.GetAxis("Vertical");
 
-        Vector3 moveVector = baseSpeed * transform.forward;
-        Vector3 inputs = new Vector3(moveHorizontal, 0.0f, moveVertical);
-
-        Vector3 yaw = inputs.x * transform.right * rotSpeedX * Time.deltaTime;
-        Vector3 pitch = inputs.z * -transform.up * rotSpeedY * Time.deltaTime;
-        Vector3 dir = yaw + pitch;
-
-        float maxX = Quaternion.LookRotation(moveVector + dir).eulerAngles.x;
-
-        if (maxX < 90 && maxX > 70 || maxX > 270 && maxX < 290)
+        if (isBall)
         {
+            gravity = 10.0f;
         }
         else
         {
-            moveVector += dir;
+            gravity = 2.0f;
 
-            transform.rotation = Quaternion.LookRotation(moveVector);
+            //Vector3 moveVector = baseSpeed * transform.forward;
+            Vector3 inputs = new Vector3(moveYaw, 0.0f, movePitch);
+
+            Vector3 yaw = inputs.x * transform.right * rotSpeedX * Time.deltaTime;
+            Vector3 pitch = inputs.z * -transform.up * rotSpeedY * Time.deltaTime;
+            Vector3 dir = yaw + pitch;
+
+            float maxX = Quaternion.LookRotation(moveVector + dir).eulerAngles.x;
+
+
+            if (maxX < 90 && maxX > 70 || maxX > 270 && maxX < 290)
+            {
+                moveVector += Vector3.down * gravity * Time.deltaTime;
+            }
+            else
+            {
+                //Vector3 vertVel = 
+                moveVector += dir;
+                moveVector -= Vector3.Magnitude(moveVector) * transform.forward;
+
+
+                transform.rotation = Quaternion.LookRotation(moveVector);
+
+            }
+            
+
+            //camera
+            Vector3 moveCam = transform.position - (transform.forward * cameraDist) + (transform.up * cameraOffset);
+            Camera.main.transform.position = Vector3.Lerp(Camera.main.transform.position, moveCam, 0.04f);
+            Camera.main.transform.LookAt(transform.position + (moveVector * 10));
+            Debug.Log(Vector3.Magnitude(moveVector));
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                controller.Move(moveVector * 5 * Time.deltaTime);
+            }
+            else
+            {
+                controller.Move(moveVector * Time.deltaTime);
+            }
+            
+
+
+
         }
-        Debug.Log("Player = " + maxX);
-        controller.Move(moveVector * Time.deltaTime);
+        
     }
 }
--- LLAC/Assets/Scripts/RotateModel.cs
+++ LLAC/Assets/Scripts/RotateModel.cs
@@ -4,22 +4,20 @@ using UnityEngine;
 
 public class RotateModel : MonoBehaviour {
     
-    [SerializeField] private Transform player;
     float smooth = 4.0f;
-    float tiltAngle = 60.0f;
+    float tiltAngle = 20.0f;
 
     void Update()
     {
         // Smoothly tilts a transform towards a target rotation.
-        float tiltAroundZ = Input.GetAxis("Horizontal") * tiltAngle*3/4;
+        float tiltAroundZ = Input.GetAxis("Horizontal") * tiltAngle;
         float tiltAroundY = Input.GetAxis("Horizontal") * tiltAngle/2;
         float tiltAroundX = Input.GetAxis("Vertical") * tiltAngle;
 
-        Quaternion target = (Quaternion.Euler(tiltAroundX, tiltAroundY, -tiltAroundZ) * player.localRotation);
+        Quaternion target = Quaternion.Euler(0, 0, -tiltAroundZ);
 
         // Dampen towards the target rotation
-        transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * smooth);
-
+        transform.localRotation = Quaternion.Slerp(transform.localRotation, target, Time.deltaTime * smooth);
         if (Input.GetKey(KeyCode.Alpha1))
         {
         }
